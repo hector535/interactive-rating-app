e96fd13982eb9ffcba02dfe43b5893c5973bdfee
Héctor Figuereo <hectorfiguereo26@gmail.com>
2022-03-27 18:24:04 -0400

    stars not showing fixed

--- src/components/StarList.js
+++ src/components/StarList.js
@@ -1,10 +1,18 @@
 import StarItem from "./StarItem";
 import classes from "./StarList.module.css";
 
+const createDummyArray = (length) => {
+  let stars = [];
+
+  for (let i = 0; i < length; i++) stars.push(<StarItem key={i} />);
+
+  return stars;
+};
+
 const StarList = (props) => {
   const { rating } = props;
 
-  const stars = new Array(rating).map((_, i) => <StarItem key={i} />);
+  const stars = createDummyArray(rating);
   return <ul className={classes["list"]}>{stars}</ul>;
 };
 
